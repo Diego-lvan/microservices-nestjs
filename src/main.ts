7f2362bd9c70b90664b759821677d9877de7ba7e
Omar Luna Hernández <42101656@uaz.edu.mx>
2024-05-27 16:43:06 -0600

    Se establece como inicia el microservicio

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
+import { ServerConstants } from './constants/server.constants';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule,{
+    transport: Transport.TCP,
+    options: {
+      host: ServerConstants.GATEWAY_HOST,
+      port: ServerConstants.GATEWAY_PORT
+    },
+  });
+  app.listen();
 }
 bootstrap();
