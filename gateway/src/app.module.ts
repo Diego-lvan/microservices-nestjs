c69361a6e5c126865c5d09da62c2e4f2c87f801d
Diego Iván <80365304+Diego-lvan@users.noreply.github.com>
2024-05-26 21:47:30 -0600

    agregando gateway

---
+++ gateway/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'SERVICE_ORDER',
+        transport: Transport.TCP,
+        options: { host: 'localhost', port: 3001 },
+      },
+
+      {
+        name: 'SERVICE_PAYMENT',
+        transport: Transport.TCP,
+        options: { host: 'localhost', port: 3002 },
+      },
+    ]),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
