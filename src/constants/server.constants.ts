8257e819db35ec2c520f0952a9265926e96b95b5
Omar Luna Hernández <42101656@uaz.edu.mx>
2024-05-27 16:25:34 -0600

    Se agregan las constantes de el host y el puerto del gateway

--- src/constants/server.constants.ts
+++ src/constants/server.constants.ts
@@ -4,4 +4,6 @@ dotenv.config();
 export class ServerConstants {
   static PORT: number = process.env.SERVER_PORT ? parseInt(process.env.SERVER_PORT) : 3002;
   static HOST: string = `${process.env.SERVER_HOST || 'http://localhost'}:${this.PORT}`;
+  static GATEWAY_HOST: string = 'localhost';
+  static GATEWAY_PORT: number = 3001;
 }
