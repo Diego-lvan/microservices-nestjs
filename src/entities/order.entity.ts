5ffed0ff275fba1b3a94237ec7ea8ad47b5a9ce7
Omar Luna Hernández <42101656@uaz.edu.mx>
2024-05-27 17:04:15 -0600

    Se agregan columnas relacionadas con el pago de la orden

--- src/entities/order.entity.ts
+++ src/entities/order.entity.ts
@@ -13,4 +13,16 @@ export class Order {
 
   @Column()
   price: number;
+
+  @Column({ default: 'NEW' })
+  status: string;
+
+  @Column({type: 'timestamp', default: () => 'CURRENT_TIMESTAMP'})
+  createTimestamp: Date;
+
+  @Column({type: 'timestamp', default: () => 'CURRENT_TIMESTAMP'})
+  updateTimestamp: Date;
+
+  @Column({default:() => 0})
+  paymentId: number; 
 }
