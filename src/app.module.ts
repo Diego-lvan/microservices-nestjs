889d36f48fbb741dfe6e04d33dd2bef6e2736ea9
Omar Luna Hernández <42101656@uaz.edu.mx>
2024-05-27 16:21:40 -0600

    Definición y registro de modulo de base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ORMconfig } from './configs/orm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ServerConstants } from './constants/server.constants';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(ORMconfig),
+    TypeOrmModule.forFeature([Order]),
+    ClientsModule.register([
+      {
+        name: "SERVICE_ORDER",
+        transport: Transport.TCP,
+        options: {
+          host: "localhost",
+          port: ServerConstants.PORT
+        }
+      }
+    ])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
