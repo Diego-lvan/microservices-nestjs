01c7d9157fe47408becedc525183bf34e4082ab2
Diego Iván <80365304+Diego-lvan@users.noreply.github.com>
2024-05-28 09:27:23 -0600

    actualizando constantes

--- order-service/src/constants/server.constants.ts
+++ order-service/src/constants/server.constants.ts
@@ -5,5 +5,5 @@ export class ServerConstants {
   static SERVICE_PAYMENT_PORT: number = 3002;
   static HOST: string = `${process.env.SERVER_HOST || 'http://localhost'}:${this.SERVICE_PAYMENT_PORT}`;
   static MICROSERVICE_PORT: number = 3001;
-  static MICROSERVICE_HOST: string = 'localhost';
+  static MICROSERVICE_HOST: string = '0.0.0.0';
 }
