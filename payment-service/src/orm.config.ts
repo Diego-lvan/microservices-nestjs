0c29f36f33ada6d42c160bfded857d4ee77932c8
Diego Iván <80365304+Diego-lvan@users.noreply.github.com>
2024-05-28 12:49:49 -0600

    cambiando host de la bd

--- payment-service/src/orm.config.ts
+++ payment-service/src/orm.config.ts
@@ -1,14 +1,15 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Payment } from './payment.entity';
 
 export const config: TypeOrmModuleOptions = {
-    type: 'mysql',
-    port: 3306,
-    username: 'root',
-    password: 'root',
-    database: 'payment',
-    host: '',
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: true,
-    retryAttempts: 100,
-    retryDelay: 5000
-}
+  type: 'mysql',
+  port: 3306,
+  username: 'root',
+  password: 'root',
+  database: 'payment',
+  host: 'mysql-payment',
+  entities: [Payment],
+  synchronize: true,
+  retryAttempts: 100,
+  retryDelay: 5000,
+};
