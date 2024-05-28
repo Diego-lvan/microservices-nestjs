75fb308af60a8d478e732fc2c1fb69992a612fe7
Omar Luna Hernández <42101656@uaz.edu.mx>
2024-05-27 20:07:56 -0600

    Se agrega un aviso donde indica donde esta corriendo el microservicio

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,11 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule,{
     transport: Transport.TCP,
     options: {
-      host: ServerConstants.GATEWAY_HOST,
-      port: ServerConstants.GATEWAY_PORT
+      host: ServerConstants.MICROSERVICE_HOST,
+      port: ServerConstants.MICROSERVICE_PORT
     },
   });
   app.listen();
+  console.log('Microservice listening on port '+ ServerConstants.MICROSERVICE_PORT);
 }
 bootstrap();
