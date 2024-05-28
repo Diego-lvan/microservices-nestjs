cb4f5829053c1532c4d54d59a894a9212eb33925
Omar Luna Hernández <42101656@uaz.edu.mx>
2024-05-27 20:06:33 -0600

    Se renombran las constantes

--- src/constants/server.constants.ts
+++ src/constants/server.constants.ts
@@ -2,8 +2,8 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export class ServerConstants {
-  static PORT: number = process.env.SERVER_PORT ? parseInt(process.env.SERVER_PORT) : 3002;
-  static HOST: string = `${process.env.SERVER_HOST || 'http://localhost'}:${this.PORT}`;
-  static GATEWAY_HOST: string = 'localhost';
-  static GATEWAY_PORT: number = 3001;
+  static SERVICE_PAYMENT_PORT: number = 3002;
+  static HOST: string = `${process.env.SERVER_HOST || 'http://localhost'}:${this.SERVICE_PAYMENT_PORT}`;
+  static MICROSERVICE_PORT: number = 3001;
+  static MICROSERVICE_HOST: string = 'localhost';
 }
